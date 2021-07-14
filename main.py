2c81c7b24691e57352a6756a23593114a47b2a74
Maxim Sereda <AfkZa4aem@users.noreply.github.com>
2021-07-14 13:36:40 +0300

    add save function

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-# ---------------------------- UI SETUP ------------------------------- #
 
+def save_password():
+    web_site = web_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+    with open("data.txt", mode="a") as file:
+        file.write(
+            f"\n{web_site} | {username} | {password}"
+        )
+    web_entry.delete(0, END)
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
 window.config(padx=40, pady=40)
@@ -26,6 +39,7 @@ password_label.grid(column=0, row=3)
 # Entries
 web_entry = Entry(width=35)
 web_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
+web_entry.focus()
 username_entry = Entry(width=35)
 username_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
 password_entry = Entry(width=33)
@@ -34,33 +48,8 @@ password_entry.grid(column=1, row=3, sticky="W")
 # Buttons
 gen_button = Button(text="Generate Password")
 gen_button.grid(column=2, row=3, sticky="EW")
-add_button = Button(text="Add", width=30)
+add_button = Button(text="Add", width=30, command=save_password)
 add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 window.mainloop()
